30998df857dec229a5f383d811cc4d8532773002
Rachel <122761172+rachellee333@users.noreply.github.com>
2026-01-01 21:21:05 +0000

    Committing changes to penguin_ml filepaths

--- streamlit_apps/penguin_ml/penguins_streamlit.py
+++ streamlit_apps/penguin_ml/penguins_streamlit.py
@@ -21,15 +21,15 @@ if password_guess != 'streamlit_password':
 penguin_df = st.file_uploader('Upload the penguin csv file')
 
 # rf_pickle = open('/workspaces/Streamlit/streamlit_apps/penguin_ml/random_forest_penguin.pickle', 'rb')
-rf_pickle = open('random_forest_penguin.pickle', 'rb')
+rf_pickle = open('streamlit_apps/penguin_ml/random_forest_penguin.pickle', 'rb')
 # map_pickle = open('/workspaces/Streamlit/streamlit_apps/penguin_ml/output_penguin.pickle', 'rb')
-map_pickle = open('output_penguin.pickle', 'rb')
+map_pickle = open('streamlit_apps/penguin_ml/output_penguin.pickle', 'rb')
 rfc = pickle.load(rf_pickle)
 unique_penguin_mapping = pickle.load(map_pickle)
 rf_pickle.close()
 map_pickle.close()
 # penguin_df = pd.read_csv('/workspaces/Streamlit/streamlit_apps/penguin_ml/penguin_ml.csv')
-penguin_df = pd.read_csv('penguin_ml.csv')
+penguin_df = pd.read_csv('streamlit_apps/penguin_ml/penguin_ml.csv')
 penguin_df = penguin_df.dropna()
 output = penguin_df['species']
 features = penguin_df[
