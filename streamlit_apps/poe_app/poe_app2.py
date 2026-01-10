73c1cdf50a4cd86fe30c771405b347534b9324a8
Rachel <122761172+rachellee333@users.noreply.github.com>
2026-01-10 18:28:29 +0000

    Committing changes to poe_app2.py beautifying response on 1/10/2026

--- streamlit_apps/poe_app/poe_app2.py
+++ streamlit_apps/poe_app/poe_app2.py
@@ -3,6 +3,8 @@ import fastapi_poe as fp
 import streamlit as st
 
 st.balloons()
+my_list = []
+my_string = ''
 
 system_message_default = 'Think of an inspiring quote'
 
@@ -20,4 +22,11 @@ if analyze_button:
   )
 
   for partial in fp.get_bot_response_sync(messages=[message], bot_name='Claude-Sonnet-4.5', api_key=api_key):
-    st.write(partial.text)
+    # st.write(partial.text)
+    my_list.append(partial.text)
+    # my_string.append(partial.text)
+
+  complete_response = ''.join(my_list)
+
+  st.write(complete_response)
+  # st.write(my_string)
